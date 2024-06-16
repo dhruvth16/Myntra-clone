961352f7fed1da5057a6b972047491d9e0a1058a
Dhruv <159543434+dhruvth16@users.noreply.github.com>
2024-06-16 08:41:34 +0530

    new component

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,7 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import axios from 'axios'
 import Navbar from './Components/Navbar';
+import ProductList from './Components/ProductList';
 
 function App() {
-  // const [img, setImg] = useState();
-  const [error, seterror] = useState(false);
-  const [api, setApi] = useState([]);
-  const [loading, setloading] = useState(false);
-
-  useEffect(() => {
-    (async () => {
-      try {
-        setloading(true)
-        seterror(false)
-        const response = await axios.get('https://fakestoreapi.com/products')
-        const data = await response.data;
-        setApi(data);
-        setloading(false)
-      } catch (error) {
-        seterror(true)
-        setloading(false)
-      }
-    })()
-  }, [])
 
   // useEffect(() => {
   //     (async () => {
@@ -42,22 +20,6 @@ function App() {
   //   } 
   // , [])
 
-  if (error) {
-    return <div className="min-h-screen flex flex-grow items-center justify-center bg-gray-50">
-      <div className="rounded-lg bg-white p-8 text-center shadow-xl">
-        <h1 className="mb-4 text-4xl font-bold">404</h1>
-        <p className="text-gray-600">Oops! The page you are looking for could not be found.</p>
-        <a href="/" className="mt-4 inline-block rounded bg-blue-500 px-4 py-2 font-semibold text-white hover:bg-blue-600"> Go back to Home </a>
-      </div>
-    </div>
-  }
-
-  if (loading) {
-    return <div className='h-screen w-full flex items-center justify-center'>
-      <div className="spinner"></div>
-    </div>
-  }
-
   return (
     <>
       <Navbar />
@@ -73,23 +35,7 @@ function App() {
           <h3 className='text-center text-3xl font-mono font-medium'>Top Deals</h3>
           <img className='w-full mb-4 hidden lg:block' src="https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2024/3/13/f7833d8c-a2b1-4aac-989b-ca397a02dfcd1710340369987-Crazy-Deals.jpg" alt="" />
         </div>
-        <div className='flex justify-center gap-8 flex-wrap p-5'>
-          {api.map(item => {
-            return <div className='' key={item.id}>
-              <div className='h-[320px] w-[270px] mb-1 bg-gradient-to-r from-pink-500 to-pink-400 flex items-center justify-center rounded-md'>
-                <img className='w-[250px] h-[300px] rounded-md hover:scale-105 transition-all' src={item.image} alt="" />
-              </div>
-              <div className='my-2'>
-                <p className='w-[200px] overflow-hidden text-ellipsis whitespace-nowrap'>{item.title}</p>
-                <p className='uppercase font-semibold'><span className='font-normal'>category: </span>{item.category}</p>
-              </div>
-              <div>
-                <p className='font-bold'><span >Price: $</span>{item.price}</p>
-                <p></p>
-              </div>
-            </div>
-          })}
-        </div>
+        <ProductList />
       </div>
     </>
   )
